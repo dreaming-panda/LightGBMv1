44d58629b7369dfc91a8d3f43b607b2a1d7991c7
Ubuntu <shiyu@gbdt-shiyu.ren3kv4wanvufliwrpy4k03lsf.xx.internal.cloudapp.net>
2019-11-04 04:40:26 +0000

    print tree size information in warning level

--- src/treelearner/serial_tree_learner.cpp
+++ src/treelearner/serial_tree_learner.cpp
@@ -233,7 +233,7 @@ Tree* SerialTreeLearner::Train(const score_t* gradients, const score_t *hessians
     #endif
     cur_depth = std::max(cur_depth, tree->leaf_depth(left_leaf));
   }
-  Log::Debug("Trained a tree with leaves = %d and max_depth = %d", tree->num_leaves(), cur_depth);
+  Log::Warning("Trained a tree with leaves = %d and max_depth = %d", tree->num_leaves(), cur_depth);
   Log::Warning("histogram time %f s", hist_time_.count());
   return tree.release();
 }
